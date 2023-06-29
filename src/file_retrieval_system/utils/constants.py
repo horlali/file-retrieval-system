ebf4658e1e81bf87a55c40b9e1c4a56dcc7eef4e
se7en <gideon.ahiadzi@gmail.com>
2023-06-29 01:57:46 +0000

    formatting

--- src/file_retrieval_system/utils/constants.py
+++ src/file_retrieval_system/utils/constants.py
@@ -4,10 +4,11 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
+load_dotenv(os.path.join(BASE_DIR, ".env"))
+
 SERVER_FILE_DIR = BASE_DIR / "files" / "server"
 CLIENT_FILE_DIR = BASE_DIR / "files" / "client"
 
-load_dotenv(os.path.join(BASE_DIR, ".env"))
 OBJECT_ID = os.getenv("OBJECT_ID")
 HOST = os.getenv("HOST")
 PORT = int(os.getenv("PORT"))
